1af6e3c4014d1dc18fadd09dd11b8f64e71338c1
Павел Васильев <Pavel.Vasilyev@itspro.by>
2020-05-14 16:16:06 +0300

    made firs page beerfest.by and make your adaptive

--- js/main.js
+++ js/main.js
@@ -1,15 +1,70 @@
   $(document).ready(function () {
 
-    var mySwiper = new Swiper ('.swiper-container', {
-      // Optional parameters
-      direction: 'horizontal',
-      loop: true,
-      pagination: {
-        el: '.swiper-pagination',
-      },
-    })
-    
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  })
+  
+  const modal = $('.menu__window');
+  $(document).keyup(function (e) { 
+    let key = e.keyCode;
+      if(key == 27) {
+        modal.toggleClass('menu__window--active');
+        $(".menu__btn").removeClass("menu__btn--active")
+  }
+  
+  });
 
+
+  var window = $('.menu__window');
+  $('.menu__bar').on('click', function(e) {
+
+      e.preventDefault;
+      $(this).children().toggleClass('menu__btn--active');
+      window.toggleClass('menu__window--active')
+  });
+
+
+  $(document).mouseup(function(e) {
+      var container_header = $(".header__mobile"); 
+      var container = $(".menu__window.menu__window--active"); 
+
+      // if the target of the click isn't the container nor a descendant of the container
+      if (!container.is(e.target) && container.has(e.target).length === 0 && 
+          !container_header.is(e.target) && container_header.has(e.target).length === 0
+      ) 
+      {
+          $(".menu__window").removeClass("menu__window--active");
+          $(".menu__btn").removeClass("menu__btn--active")
+      }
+  });
+  //наведение на иконки футера
+  $('.social__image--instagram').hover(function () {
+      $('.social__image--instagram').css('display', 'none');
+      $('.social__image--instagram-active').css('display', 'flex');
+      
+    }, function () {
+
+      $('.social__image--instagram').css('display', 'flex');
+      $('.social__image--instagram-active').css('display', 'none');
+
+    }
+  );
+  $('.social__image--google').hover(function () {
+    $('.social__image--google').css('display', 'none');
+    $('.social__image--google-active').css('display', 'flex');
+    
+  }, function () {
+
+    $('.social__image--google').css('display', 'flex');
+    $('.social__image--google-active').css('display', 'none');
+
+  }
+);
 
   
 
@@ -18,10 +73,8 @@
 
 
 
-
-
   // mask phone
-  $('[type=tel]').mask('+7 000 000-00-00', {placeholder: "+7 ___ ___-__-__"});
+  $('[type=tel]').mask('+7 000 000-00-00', {placeholder: "+375 __ ___-__-__"});
 
   new WOW().init(); 
 
