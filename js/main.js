dac7757347c026fac7b240e7effeff4adc6e8d73
Павел Васильев <Pavel.Vasilyev@itspro.by>
2020-05-18 17:57:38 +0300

    added interactive from js for cards

--- js/main.js
+++ js/main.js
@@ -98,28 +98,11 @@
 
 
 
-  
-  // function showDialog(text) {
-  //   $('#row-content').html(text);
-  //   $('.modal').modal('show');
-  // }
-  
-
-
-  $(".card").click(function() {
-    //открыть модальное окно с id="myModal"
-    $(".modal").modal('card');
-  });
-
-
-
-
-
 
   // mask phone
   $('[type=tel]').mask('+7 000 000-00-00', {placeholder: "+375 __ ___-__-__"});
 
-  new WOW().init(); 
+  // new WOW().init(); 
 
   //Smooth Scrolling Using Navigation Menu
   // $('a[href*="#"]').on('click', function(e){
@@ -135,43 +118,65 @@
 
   //работа с карточками 
       
-  const cards = document.querySelector ('.crad');
+  // const cards = document.querySelector('.card');
 
-  function createCard() {
-    const card = `
-    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-                <div class="cards menu__cards">
-                  <div class="crad" data-toggle="modal" data-target="#exampleModal">
-                    <img src="img/menu/fokasna.png" alt="fokasna" class="card__image">
-                    <h2 class="card__title">Фокасна</h2>
-                    <div class="card__price">
-                      <span class="card__price--actual">1,75 BYN</span>
-                      <span class="card__price--full">2,25 BYN</span>
-                    </div>
-                  </div>
-                  <div class="orders card__orders">
-                    <div class="orders__click">
-                      <button class="orders__btn"><span class="orders__btn--minus">-</span></button>
-                      <span class="orders__number">1</span>
-                      <button class="orders__btn"><span class="orders__btn--plus">+</span></button>
-                    </div>
-                    <button class="order">
-                      <img src="img/header/basket.svg" alt="basket" class="order__image">
-                      <span class="order__text">добавить</span>
-                    </button>
-                  </div>
-                </div>
-              </div>
-    `;
+  // function createCard() {
+  //   var card = ``;
+  //   cards.insertAdjacentHTML('beforebegin', card);
+  //   console.log('ok');
+    
+  // }
 
-    cards.insertAdjacentHTML('beforebegin', card );
 
+
+  function showDialog(text) {
+    $('#content').html(text);
+    $('#exampleModal').modal('show');
   }
   
-  
-  cards();
+  $(".card").click(function() {
+    var imgSrc = $(this).find('.card__image').attr('src');
+    var titleCard = $(this).find('.card__title').text();
+    var priceCardActual = $(this).find('.card__price--actual').text();
+    var priceCardFull = $(this).find('.card__price--full').text();
+    var modalCard = 
+    `<div class="col-lg-6"><img src="${imgSrc}"></div>
+    <div class="col-lg-6">
+      <h2 class="card__title">${titleCard}</h2>
+        <div class="card__price">
+          <span class="card__price--actual">${priceCardActual}</span>
+          <span class="card__price--full">${priceCardFull}</span>
+        </div>
+        <div class="orders card__orders">
+        <div class="orders__click">
+          <button class="orders__btn"><span class="orders__btn--minus">-</span></button>
+          <span class="orders__number">1</span>
+          <button class="orders__btn"><span class="orders__btn--plus">+</span></button>
+        </div>
+        <button class="order">
+          <img src="img/header/basket.svg" alt="basket" class="order__image">
+          <span class="order__text">добавить</span>
+        </button>
+      </div>
 
-  createCard();
+    </div>`
+
+
+    console.log(modalCard);
+    showDialog(modalCard);
+  });
+
+
+  
+  
+
+
+
+
+
+  
+
+
 
 
 });
